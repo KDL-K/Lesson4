682c7d0a0967a71eef24de352d1afd4ddd9c0ae0
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-07-04 15:58:57 +0300

    Task11_101
    Finish

---
+++ Task11_101.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Task11_101 {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Please input the size of array: ");
+        int sizeArray=in.nextInt();
+        int[] arrayT=new int[sizeArray];
+        for(int i=0;i<sizeArray;i++){
+            arrayT[i]=(int)(Math.random()*10);
+        }
+        System.out.println("\tArray:");
+        for(int i=0;i<sizeArray;i++){
+            System.out.print(arrayT[i]+" ");
+        }
+        System.out.println();
+        
+        int count;
+        for(int i=0;i<sizeArray-1;i++){
+            for(int j=i+1;j<sizeArray;j++){
+                if(arrayT[i]==arrayT[j]) count++;
+            }
+        }        
+        if(count==1) System.out.println("\Array has only 2 repeating numbers");
+        else if(count==0) System.out.println("\Array hasn't any repeating numbers");
+             else System.out.println("\Array has more than 2 repeating numbers");
+        
+        
+}
